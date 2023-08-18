04b304ef82ab048f0276054b743ec752733ddab8
Wonmin Choi <min5024@naver.com>
2023-08-18 22:29:50 +0900

    23-08-18 backup before change

--- lib/pages/p_words.dart
+++ lib/pages/p_words.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:arabic_korean_memo/layouts/w_memorized_button.dart';
-import 'package:arabic_korean_memo/layouts/w_word_cards.dart';
+import 'package:arabic_korean_memo/widgets/w_memorized_button.dart';
+import 'package:arabic_korean_memo/widgets/w_word_cards.dart';
 import 'package:arabic_korean_memo/themes/my_icons.dart';
 
 class MainPageWords extends StatefulWidget {
