8a06d4763524dbd6b9544971f4d65ac9397e020b
Wonmin Choi <min5024@naver.com>
2023-08-21 02:55:46 +0900

    23-08-20 card click button

--- lib/pages/pp_flash_cards.dart
+++ lib/pages/pp_flash_cards.dart
@@ -1,3 +1,4 @@
+import 'package:arabic_korean_memo/themes/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:arabic_korean_memo/widgets/w_csv_to_item.dart';
@@ -68,78 +69,157 @@ class _FlashCardsState extends State<FlashCards> {
         ],
       ),
       body: Center(
-        child: SizedBox(
-          height: 400,
-          width: 300,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: SwipableStack(
-                    itemCount: _data.length,
-                    detectableSwipeDirections: const {
-                      SwipeDirection.left,
-                      SwipeDirection.right,
-                    },
-                    controller: _controller,
-                    stackClipBehaviour: Clip.none, // ??
-                    horizontalSwipeThreshold: 0.8,
-
-                    onSwipeCompleted: (index, direction) {
-                      if (direction == SwipeDirection.left) {
-                        print('Left $index'); // Print 'Left' when swiped left
-                      } else if (direction == SwipeDirection.right) {
-                        print(
-                            'Right $index'); // Print 'Right' when swiped right
-                      }
-                    },
-                    builder: (context, swipeProperty) {
-                      final index = swipeProperty.index;
-                      // if (index == _data.length + 1) {
-                      //   print('done');
-                      //   return Container(); // Leave this empty for now
-                      // }
-                      final item = _data[index];
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _showArabic = !_showArabic;
-                          });
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 400,
+              width: 300,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: SwipableStack(
+                        itemCount: _data.length,
+                        detectableSwipeDirections: const {
+                          SwipeDirection.left,
+                          SwipeDirection.right,
                         },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.amber,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 3,
-                                blurRadius: 5,
-                              )
-                            ],
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          height: 400,
-                          width: 300,
-                          child: Center(
-                            child: Text(
-                              _showArabic
-                                  ? item.arabicWord
-                                  : item.koreanMeaning,
-                              style: const TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
+                        controller: _controller,
+                        stackClipBehaviour: Clip.none, // ??
+                        horizontalSwipeThreshold: 0.8,
+
+                        overlayBuilder: (context, swipeProperty) {
+                          if (swipeProperty.direction == SwipeDirection.right &&
+                              swipeProperty.swipeProgress > 0.75) {
+                            return const Positioned(
+                              top: 16,
+                              right: 16,
+                              child: Icon(Icons.circle_outlined,
+                                  size: 32, color: Colors.green),
+                            );
+                          } else if (swipeProperty.direction ==
+                                  SwipeDirection.left &&
+                              swipeProperty.swipeProgress > 0.75) {
+                            return const Positioned(
+                              top: 16,
+                              left: 16,
+                              child: Icon(Icons.clear_outlined,
+                                  size: 32, color: Colors.red),
+                            );
+                          } else {
+                            return const SizedBox();
+                          }
+                        },
+
+                        onSwipeCompleted: (index, direction) {
+                          if (direction == SwipeDirection.left) {
+                            print(
+                                'Left $index'); // Print 'Left' when swiped left
+                          } else if (direction == SwipeDirection.right) {
+                            print(
+                                'Right $index'); // Print 'Right' when swiped right
+                          }
+                        },
+                        builder: (context, swipeProperty) {
+                          final index = swipeProperty.index;
+                          // if (index == _data.length + 1) {
+                          //   print('done');
+                          //   return Container(); // Leave this empty for now
+                          // }
+                          final item = _data[index];
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _showArabic = !_showArabic;
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: hermesOrange,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 3,
+                                    blurRadius: 5,
+                                  )
+                                ],
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              height: 400,
+                              width: 300,
+                              child: Center(
+                                child: Text(
+                                  _showArabic
+                                      ? item.arabicWord
+                                      : item.koreanMeaning,
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      );
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Ink(
+                  decoration: const ShapeDecoration(
+                    shape: CircleBorder(),
+                    color: hermesOrange,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.clear_outlined),
+                    iconSize: 40,
+                    color: Colors.white,
+                    onPressed: () {
+                      _controller.next(swipeDirection: SwipeDirection.left);
                     },
                   ),
                 ),
-              ),
-            ],
-          ),
+                Ink(
+                  decoration: const ShapeDecoration(
+                    shape: CircleBorder(),
+                    color: hermesOrange,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.refresh_outlined),
+                    iconSize: 40,
+                    color: Colors.white,
+                    onPressed: () {
+                      //refresh
+                    },
+                  ),
+                ),
+                Ink(
+                  decoration: const ShapeDecoration(
+                    shape: CircleBorder(),
+                    color: hermesOrange,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.circle_outlined),
+                    iconSize: 37,
+                    color: Colors.white,
+                    onPressed: () {
+                      _controller.next(swipeDirection: SwipeDirection.right);
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
